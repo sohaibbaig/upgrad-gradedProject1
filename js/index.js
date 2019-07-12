bde518edb7b4214c6306ae45f9f978ae0246509d
sohaibbaig01 <sohaibbaig01@gmail.com>
2019-07-12 14:22:48 +0530

    added create Post form

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function showCreatePost(){
+    let createPost = document.getElementById('modalCreatePost');
+    createPost.style.display = 'block';
+}
+function hideCreatePost(){
+    let createPost = document.getElementById('modalCreatePost');
+    createPost.style.display = 'none';
+}
